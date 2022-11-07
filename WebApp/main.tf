4e45d6ddd3f34b9af7e318fe91f4b408508b9d93
Nizar EL Ouarti <46408023+Nizarel@users.noreply.github.com>
2022-11-07 23:02:07 +0000

    impr master2

--- WebApp/main.tf
+++ WebApp/main.tf
@@ -7,8 +7,8 @@ resource "azurerm_service_plan" "example" {
   name                = var.app_service_plan_name
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
+  os_type             = var.app_service_os_type
+  sku_name            = var.app_service_namer.app_service_plan_sku
 }
 
 resource "azurerm_linux_web_app" "example" {
